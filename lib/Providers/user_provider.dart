ac6fa275a148542d871b0898aada6938db0f244c
Fasica Zewdie <fasica.zewdie7@gmail.com>
2022-07-12 21:26:31 +0300

    UI for search

--- lib/Providers/user_provider.dart
+++ lib/Providers/user_provider.dart
@@ -4,12 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class UserProvider with ChangeNotifier {
+  Map _userInfo = {};
+
+  Map get userInfo {
+    return _userInfo;
+  }
+
   Future<void> fetchUser(String userName) async {
     final url = Uri.parse('https://api.github.com/users/$userName');
     try {
       var response = await http.get(url);
       var extractedData = json.decode(response.body);
-      print(extractedData.toString());
+      print('ffffffffffffffffffffffffffffffffffffff');
+      _userInfo = extractedData as Map;
+      print(_userInfo.toString());
     } catch (error) {}
+    notifyListeners();
   }
 }
--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -13,6 +13,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TextEditingController _searchController = TextEditingController();
+  Map _userInfo = {};
+  bool _isLoading = false;
   @override
   void didChangeDependencies() async {
     // TODO: implement didChangeDependencies
@@ -20,8 +22,16 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> _search() async {
-    await Provider.of<UserProvider>(context, listen: false)
-        .fetchUser(_searchController.text);
+    setState(() {
+      _isLoading = true;
+    });
+    var provider = Provider.of<UserProvider>(context, listen: false);
+
+    await provider.fetchUser(_searchController.text);
+    _userInfo = provider.userInfo;
+    setState(() {
+      _isLoading = false;
+    });
   }
 
   final _outlineInputBorder = const OutlineInputBorder();
@@ -32,53 +42,106 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: size.width * 0.6,
-                child: TextFormField(
-                  controller: _searchController,
-                  textInputAction: TextInputAction.next,
-                  autovalidateMode: AutovalidateMode.onUserInteraction,
-                  decoration: InputDecoration(
-                    contentPadding: _contentPadding,
-                    border: _outlineInputBorder,
-                    prefixIcon: Icon(Icons.search),
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: SafeArea(
+        child: Container(
+          color: Colors.blueGrey[800],
+          padding: EdgeInsets.all(10),
+          child: Column(children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.white,
+                  width: size.width * 0.6,
+                  child: TextFormField(
+                    controller: _searchController,
+                    textInputAction: TextInputAction.next,
+                    autovalidateMode: AutovalidateMode.onUserInteraction,
+                    decoration: InputDecoration(
+                      contentPadding: _contentPadding,
+                      border: _outlineInputBorder,
+                      prefixIcon: Icon(
+                        Icons.search,
+                      ),
+                    ),
+
+                    // validator: (val) {
+                    //   if (val.toString().isEmpty && txt != 'Tag') {
+                    //     return '$txt is required';
+                    //   } else if ((txt == 'Tag') && (tags.isEmpty)) {
+                    //     return 'tag is required';
+                    //   }
+                    //   return null;
+                    // },
+                    // onChanged: (value) {
+                    //   setState(() {});
+                    // },
+                    // onSaved: (value) {
+
+                    // },
                   ),
-
-                  // validator: (val) {
-                  //   if (val.toString().isEmpty && txt != 'Tag') {
-                  //     return '$txt is required';
-                  //   } else if ((txt == 'Tag') && (tags.isEmpty)) {
-                  //     return 'tag is required';
-                  //   }
-                  //   return null;
-                  // },
-                  // onChanged: (value) {
-                  //   setState(() {});
-                  // },
-                  // onSaved: (value) {
-
-                  // },
                 ),
-              ),
-              Container(
-                width: size.width * 0.3,
-                child: ElevatedButton(
-                  child: Text('Search'),
-                  onPressed: _search,
-                ),
-              )
-            ],
-          )
-        ]),
+                Container(
+                  width: size.width * 0.3,
+                  child: ElevatedButton(
+                    child: Text('Search'),
+                    onPressed: _search,
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            if (_searchController.text.isNotEmpty)
+              _isLoading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Center(
+                      child: Container(
+                        child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              CircleAvatar(
+                                child: Image.network(_userInfo['avatar_url']),
+                              ),
+                              SizedBox(width: 20),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceAround,
+                                    children: [
+                                      Text(
+                                        _userInfo['name'],
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 20),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Text('Joined ${_userInfo['created_at']}')
+                                    ],
+                                  ),
+                                  Text(
+                                    _userInfo['login'],
+                                    style: TextStyle(
+                                      color: Colors.blue,
+                                    ),
+                                  )
+                                ],
+                              ),
+                            ]),
+                      ),
+                    ),
+          ]),
+        ),
       ),
     );
   }
